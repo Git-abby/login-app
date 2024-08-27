6873d54ed44d02ca2f3ef79866a3348c8bf34a6e
Git-abby <malekabrar56@gmail.com>
2024-08-27 15:44:08 -0400

    T: Login and Register with firebase

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,48 +1,102 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-import { doSignInWithEmailAndPassword, doSignInWithGoogle } from '../../firebase/auth'
-import { useAuth } from '../../contexts/authContexts'
+import {
+  doSignInWithEmailAndPassword,
+  doSignInWithGoogle,
+} from "../../firebase/auth";
+import { useAuth } from "../../contexts/authContexts";
 
 function Login() {
-
   const { userLoggedIn } = useAuth() || false;
-  
+
   // console.log(useAuth);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const [isSigningIn, setIsSigningIn] = useState(false);
-  const [erroeMessage, setErrorMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState("");
 
-  // console.info(email, password);
+  console.info(email, password);
 
   const onSubmit = async (e) => {
     e.preventDefault();
-
-
-  }
+    if (!isSigningIn) {
+      setIsSigningIn(true);
+      try {
+        await doSignInWithEmailAndPassword(email, password);
+      } catch (error) {
+        console.log(error.message)
+        setErrorMessage(error.message);
+        setIsSigningIn(false);
+      }
+      // await doSignInWithEmailAndPassword(email, password);
+    }
+  };
+  const onGoogleSUbmit = (e) => {
+    e.preventDefault();
+    if (!isSigningIn) {
+      setIsSigningIn(true);
+      doSignInWithGoogle().catch((err) => {
+        setIsSigningIn(false);
+      });
+    }
+  };
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
-      <h2 className="text-2xl font-bold text-gray-700 mb-6">Login</h2>
-      <form className="bg-white p-6 rounded shadow-md w-full max-w-sm">
-        <div className="mb-4">
-          <label htmlFor="email" className="block text-gray-700 font-bold mb-2">Email</label>
-          <input type="email" id="email" className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600" onClick={(e) => setEmail(e.target.value)}/>
+    <>
+      {userLoggedIn && (
+        <div>
+          <h1>Welcome, User!</h1>
+          <p>You are now logged in.</p>
         </div>
-        <div className="mb-4">
-          <label htmlFor="password" className="block text-gray-700 font-bold mb-2">Password</label>
-          <input type="password" id="password" className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600" onClick={(e) => setPassword(e.target.password)} />
-        </div>
-        <button type="submit" className="bg-blue-600 text-white w-full py-2 rounded-lg hover:bg-blue-500 transition duration-300">Login</button>
-      </form>
-      <p className="mt-4">
-        <Link to="/register" className='text-blue-600 hover:underline'>
-        Don't have an account? Register
-        </Link>
-      </p>
-    </div>
-  )
+      )}
+      {errorMessage && (
+        <div className="mb-4 text-red-600 text-sm">{errorMessage}</div>
+      )}
+      <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+        <h2 className="text-2xl font-bold text-gray-700 mb-6">Login</h2>
+        <form className="bg-white p-6 rounded shadow-md w-full max-w-sm">
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 font-bold mb-2">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block text-gray-700 font-bold mb-2">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button
+            type="button"
+            onClick={onSubmit}
+            className="bg-blue-600 text-white w-full py-2 rounded-lg hover:bg-blue-500 transition duration-300">
+            Login
+          </button>
+        </form>
+        <p className="mt-4">
+          <Link to="/register" className="text-blue-600 hover:underline">
+            Don't have an account? Register
+          </Link>
+        </p>
+      </div>
+    </>
+  );
 }
 
-export default Login
+export default Login;
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { doCreateUserWithEmailAndPassword } from "../../firebase/auth";
 
 function Register() {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+ const [isRegistering, setIsRegistering] = useState(false);
+ const [errorMessage, setErrorMessage] = useState('');
+  // console.log(name, email, password)
+
+  const onSubmit = async (e) =>{
+    e.preventDefault();
+    if(!isRegistering){
+      setIsRegistering(true);
+      await doCreateUserWithEmailAndPassword(email, password);
+      // console.log(res);
+    }
+  }
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
       <h2 className="text-2xl font-bold text-gray-700 mb-6">Register</h2>
@@ -13,6 +30,7 @@ function Register() {
           <input
             type="text"
             id="name"
+            onChange={(e) => setName(e.target.value)}
             className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
           />
         </div>
@@ -23,6 +41,7 @@ function Register() {
           <input
             type="email"
             id="email"
+            onChange={(e) => setEmail(e.target.value)}
             className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
           />
         </div>
@@ -35,11 +54,13 @@ function Register() {
           <input
             type="password"
             id="password"
+            onChange={(e) => setPassword(e.target.value)}
             className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
           />
         </div>
         <button
-          type="submit"
+          type="button"
+          onClick={onSubmit}
           className="bg-blue-600 text-white w-full py-2 rounded-lg hover:bg-blue-500 transition duration-300">
           Register
         </button>
